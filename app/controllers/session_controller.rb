2bb22245a2a7497a1815592019b4ae4cbaf9db99
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2019-09-19 00:57:59 +0200

    login permanente da testare

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,6 +8,8 @@ class SessionController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember user
       redirect_to user
     else
       # Create an error message.
@@ -17,7 +19,7 @@ class SessionController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
   
