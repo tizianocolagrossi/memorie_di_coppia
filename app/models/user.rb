58c71705a26602b4cea38e63ef98d92bb77fe787
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2019-09-18 19:18:20 +0200

    messaggi errore form registrazione e se buon fine reindirizzamento alla pagina home dell utente

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
+    before_save { self.username = self.herName + "&" + self.hisName}
     validates :hisName, presence: true, length: { maximum: 20 }
     validates :herName, presence: true, length: { maximum: 20 }
     validates :anniversary, presence: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
-    validates :username, presence: true, length: { maximum: 40 }, uniqueness: { case_sensitive: false }
+    validates :email, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
     has_secure_password
     validates :password, length: { minimum: 6 }
