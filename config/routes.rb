3f5a1123bfdf68a6ba2549e0785c3aa5c312e7a1
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2019-09-18 22:55:30 +0200

    login logout implementati non ancora rememberme

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'signup' => 'users#new'
+  get 'login' => 'session#new'
+  post 'login' => 'session#create'
+  delete 'logout' => 'session#destroy'
+  get 'logout' => "session#destroy"
   root :to => 'pages#home'
-  get 'sign_up' => 'users#new'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
