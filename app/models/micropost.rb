8ff4cebdc3f87dd785023d4de63d647543a6c8d5
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2019-09-22 15:01:53 +0200

    idem

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
 
-  if imagedb
+  if imagedb == nil
     before_save { self.content_type = imagedb.content_type }
     before_save { self.filename = sanitize_filename(imagedb.original_filename) }
     before_save { self.imagedb = imagedb.read }
