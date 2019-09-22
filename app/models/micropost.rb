777d522895df867abca39c27f35c5d3b2a785a15
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2019-09-22 15:40:20 +0200

    idem

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
 
-  init_data_image(imagedb)
+  before_save :set_image_data, if: :image_is_here?
 
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
@@ -10,18 +10,20 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 5000 }
 
   private
-  def sanitize_filename(filename)
-    # Get only the filename, not the whole path (for IE)
-    # Thanks to this article I just found for the tip: http://mattberther.com/2007/10/19/uploading-files-to-a-database-using-rails
-    return File.basename(filename)
-  end
-
-  def init_data_image(image)
-    if image
-      before_save { self.content_type = imagedb.content_type }
-      before_save { self.filename = sanitize_filename(imagedb.original_filename) }
-      before_save { self.imagedb = imagedb.read }
+    def sanitize_filename(filename)
+      # Get only the filename, not the whole path (for IE)
+      # Thanks to this article I just found for the tip: http://mattberther.com/2007/10/19/uploading-files-to-a-database-using-rails
+      return File.basename(filename)
     end
-  end
+
+    def set_image_data
+      self.content_type = imagedb.content_type
+      self.filename = sanitize_filename(imagedb.original_filename)
+      self.imagedb = imagedb.read
+    end
+
+    def image_is_here? 
+      return imagedb
+    end
 
 end
